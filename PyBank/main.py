cc5f22ce121d6b51c835a47b2aa0e34a9c0a768b
jgausachs <jgausachs@yahoo.com>
2022-12-01 21:25:57 +1100

    Updated files

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,17 +6,21 @@ import csv
 csv_budget = os.path.join("Resources", "budget_data.csv" )
 
 # Variable to track row count in csv_budget including header
-rowCount = 0
+profit_loss = []
+# rowCount = 0
 
 # Open the .csv file
 with open(csv_budget, encoding="utf") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     # Loop through file counting the rows
-    for rows in csvreader:
-        rowCount += 1
+    for row in csvreader:
+        profit_loss.append(row[1])
+        # rowCount += 1
 
-print("Financial Analysis")
+print("\nFinancial Analysis")
 print("-----------------------------")
 
-print("Total Months: " + str(rowCount - 1))  # Minus 1 to remove header
+# print("Total Months: " + str(rowCount - 1))  # Minus 1 to remove header
+print(f"Total Months: " + str(len(profit_loss)))  # Minus 1 to remove header
+
